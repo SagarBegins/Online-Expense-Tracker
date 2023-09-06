8884090de03428bf558159e7d8f5ff3d97df10ac
SagarBegins <141713918+SagarBegins@users.noreply.github.com>
2023-09-06 14:00:18 +0530

    Add files via upload

---
+++ OnlineExpenseTracker/src/main/java/servlet/DeleteExpenseServlet.java
@@ -0,0 +1,37 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.ExpenseDao;
+import db.HibernateUtil;
+
+@WebServlet("/deleteExepnse")
+public class DeleteExpenseServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int id = Integer.parseInt(req.getParameter("id"));
+
+		ExpenseDao dao = new ExpenseDao(HibernateUtil.getSessionFactory());
+		boolean f = dao.deleteExpense(id);
+
+		HttpSession session = req.getSession();
+
+		if (f) {
+			session.setAttribute("msg", "Delete Sucessfully");
+			resp.sendRedirect("user/view_expense.jsp");
+		} else {
+			session.setAttribute("msg", "something wrong on server");
+			resp.sendRedirect("user/view_expense.jsp");
+		}
+
+	}
+
+}
